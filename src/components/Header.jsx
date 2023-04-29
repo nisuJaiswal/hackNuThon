91a00854e8b8aae704d9af918d72a99a03dc4974
Nisarg Jaiswal <nisujaiswal4@gmail.com>
2023-04-29 17:22:26 +0530

    Upload Container almost completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,20 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import GlobalStyles from "@mui/material/GlobalStyles";
-import { Box, Modal } from "@mui/material";
-import Dropzone from "react-dropzone";
+import { Box, Modal, Stack } from "@mui/material";
+import Dropzone, { useDropzone } from "react-dropzone";
 
 const style = {
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
+  // width: 500,
+  // height: 300,
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
@@ -21,9 +22,19 @@ const style = {
 };
 
 const Header = () => {
+  const onDrop = useCallback((acceptedFiles) => {
+    if (acceptedFiles) {
+      setName(acceptedFiles[0].name);
+      setFiles([...acceptedFiles]);
+    }
+  }, []);
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const [name, setName] = useState("");
+  const [files, setFiles] = useState([]);
   return (
     <>
       <GlobalStyles
@@ -54,32 +65,76 @@ const Header = () => {
           >
             Upload
           </Button>
+
           <Modal
             open={open}
             onClose={handleClose}
             aria-labelledby="modal-modal-title"
             aria-describedby="modal-modal-description"
           >
-            {/* <Dropzone>
-              {({ getRootProps, getInputProps }) => (
-                <div {...getRootProps()}>
-                  <input {...getInputProps()} />
-                  <p>Drag 'n' drop some files here, or click to select files</p>
-                </div>
-              )}
-            </Dropzone> */}
-            <Box sx={style}>
+            <Box
+              sx={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                bgcolor: "background.paper",
+                border: "2px solid #000",
+                boxShadow: 24,
+                p: 4,
+                height: { xs: "400px", lg: "300px" },
+                width: { xs: "300px", lg: "500px" },
+              }}
+              // sx={{ xs: {}, sm: {} }}
+            >
               <Typography id="modal-modal-title" variant="h6" component="h2">
                 Drop file here
               </Typography>
-              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                <Dropzone onDragOver={() => console.log("Dragovered")}>
-                  {({ getRootProps, getInputProps }) => (
-                    <div {...getRootProps()}>
+              <Typography
+                component={"div"}
+                id="modal-modal-description"
+                sx={{ mt: 2 }}
+              >
+                <Dropzone>
+                  {() => (
+                    <Stack
+                      {...getRootProps()}
+                      justifyContent={"center"}
+                      alignItems={"center"}
+                      style={{
+                        position: "absolute",
+                        inset: 60,
+                        borderRadius: 20,
+                        heigth: "100%",
+                        border: "2px dashed blue",
+                      }}
+                    >
                       <input {...getInputProps()} />
-                    </div>
+                      <img
+                        src={"/file.svg"}
+                        style={{ width: "70px", margin: "1rem" }}
+                      />
+
+                      {files.length ? (
+                        <Typography variant="h6"> {name} </Typography>
+                      ) : (
+                        <Typography variant="subtitle1">
+                          Drop files here or{" "}
+                          <Typography
+                            component={"span"}
+                            variant="subtitle1"
+                            color="primary"
+                            style={{ cursor: "pointer" }}
+                          >
+                            Click here{" "}
+                          </Typography>
+                          to Upload
+                        </Typography>
+                      )}
+                    </Stack>
                   )}
                 </Dropzone>
+                {/* <Button variant="contained">Upload</Button> */}
               </Typography>
             </Box>
           </Modal>
